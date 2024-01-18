b67b4afe3dfb1059f9dec10871f770947bec3cfd
Udezi Oghenero <udezioghenero@mail.com>
2024-01-18 16:46:11 +0100

    custom year

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 AOS.init({
     duration: 3000
 });
+
+const yearElements = document.querySelectorAll('.year');
+let date = new Date();
+let year = date.getFullYear();
+yearElements.forEach(element => {
+  element.innerHTML = year;
+});
